fc23bae0f24baf1de30f1539ae7915d067a656a5
Wesley Bosse <wes.bosse@gmail.com>
2017-11-09 23:06:25 -0800

    working on noscript handling, httperror

--- pgAutomation.py
+++ pgAutomation.py
@@ -1,8 +1,19 @@
 import mechanize
+import cookielib
 import scipy
+import urllib2
+import time
 
-#initialize mechanize
+#initialize mechanize and cookie jar
 br = mechanize.Browser()
+cj = cookielib.LWPCookieJar()
+
+br.set_cookiejar(cj)
+br.set_handle_equiv(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+br.set_handle_redirect(mechanize.HTTPRedirectHandler)
 
 #open press ganey portal
 br.open('https://identity.pressganey.com/')
@@ -13,12 +24,24 @@ br.form = list(br.forms())[0]
 email = br.form.find_control("Email")
 email.value = 'colleen.bosse@sharp.com'
 password = br.form.find_control("Password")
-password.value = 'colleen.bosse@sharp.com'
+password.value = '#Reports4me'
 
-#submit login
+#submit login and wait for response
 req = br.submit()
-print(br)
+time.sleep(2)
+print(req.read())
 
+#PG 'noscript' handling
+br.form = list(br.forms())[0]
+requestURL = br.form.action
+request = mechanize.Request(requestURL)
+try:
+    response = mechanize.urlopen(request)
+except urllib2.HTTPError as e:
+    print(e.read())
+
+
+print(response.read())
 #navigate to the reports
 
 #select report
